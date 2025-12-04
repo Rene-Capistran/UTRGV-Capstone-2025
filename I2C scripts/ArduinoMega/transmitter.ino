6bae608ffb19e9d11196d39178ffa831d4886b67
mlite33 <hmartinez252005@gmail.com>
2025-12-04 15:01:13 -0600

    Update transmitter.ino

--- I2C scripts/ArduinoMega/transmitter.ino
+++ I2C scripts/ArduinoMega/transmitter.ino
@@ -1,21 +1,28 @@
 #include <Wire.h>
 
-String message = "Hello world!";
-// String message = "The quick brown fox jumps over the lazy dog.";
+// String message = "Hello world!";
+String message = "The quick brown fox jumps over the lazy dog.";
 // String message = "In telecommunication and data transmission, serial communication is the process of sending data one bit at a time, sequentially, over a communication channel or computer bus.";
 
 void setup() {
-  Wire.begin();  // I2C master
+  Wire.begin();
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.print("Sending: ");
-  Serial.println(message);
+  const char* msg = message.c_str();
+  int len = message.length();
 
-  Wire.beginTransmission(8);
-  Wire.write(message.c_str());
-  Wire.endTransmission();
+  // Send in ~28 byte chunks
+  for (int i = 0; i < len; i += 28) {
+    Wire.beginTransmission(8);
+
+    int chunkLen = min(28, len - i);
+    Wire.write((uint8_t*)(msg + i), chunkLen);
+
+    Wire.endTransmission();
+    delay(3);   // allow slave time to process
+  }
 
   delay(200);
 }
